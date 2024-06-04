1a1dcea0b273bc8b9a2af9affd0073a64d3c4025
MartinRyannn <139060230+MartinRyannn@users.noreply.github.com>
2024-06-04 10:35:50 +0300

    Add files via upload

---
+++ portfolio/scripts/open2.js
@@ -0,0 +1,6 @@
+function showAboutPopup2(){
+    frame = document.getElementById("container2").style.display = "flex"; 
+}
+function closePopup2(){
+    frame = document.getElementById("container2").style.display = "none"; 
+}
